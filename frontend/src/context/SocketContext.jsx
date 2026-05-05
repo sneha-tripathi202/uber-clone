90ee193149669f3075d2c8107044b98f87758821
Sneha Tripathi <st0744397@gmail.com>
2026-05-06 00:04:39 +0530

    first commit

---
+++ frontend/src/context/SocketContext.jsx
@@ -0,0 +1,31 @@
+
+import React, { createContext, useEffect } from 'react';
+import { io } from 'socket.io-client';
+
+export const SocketContext = createContext();
+
+const socket = io(`${"http://localhost:4000"}`); // Replace with your server URL
+
+const SocketProvider = ({ children }) => {
+    useEffect(() => {
+        // Basic connection logic
+        socket.on('connect', () => {
+            console.log('Connected to server');
+        });
+
+        socket.on('disconnect', () => {
+            console.log('Disconnected from server');
+        });
+
+    }, []);
+
+
+
+    return (
+        <SocketContext.Provider value={{ socket }}>
+            {children}
+        </SocketContext.Provider>
+    );
+};
+
+export default SocketProvider;
